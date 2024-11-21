1549dd1eb31c71f175db26354e0dc6e450ab9b14
한은정 <141193305+onesilver04@users.noreply.github.com>
2024-11-22 00:47:45 +0900

    create Tables

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/auth'); // 경로 설정
+const authRoutes = require('./routes/auth');
 const viewRoutes = require('./routes/view');
 const editInfoRoutes = require('./routes/editInfo');
+const { createTables } = require('./controllers/createTables');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// MySQL 연결
-const connection = require('./db/connection');
-
+// 테이블 생성 실행
+createTables();
+// body-parser 설정
+app.use(bodyParser.json());
 // 기본 경로 - 메인 페이지 표시
 app.get('/', (req, res) => {
   res.send(`
-    <h2>정보 등록</h2>
+    <h2>Main page</h2>
     <button onclick="window.location.href='/register-consumer'">1. 손님 회원가입</button>
     <button onclick="window.location.href='/register-owner'">2. 사장님 회원가입</button>
     <button onclick="window.location.href='/login-consumer'">3. 손님 로그인</button>
